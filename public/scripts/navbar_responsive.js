68cbfcc1951a90e08a0d8f4c2fc27e110eb5d78c
William <mack.mindenhall@gmail.com>
2020-07-26 14:43:37 +0000

    Moved navbar to partial file

--- public/scripts/navbar_responsive.js
+++ public/scripts/navbar_responsive.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function () {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
+
+  $(document).on("click", ".navbar-burger", function (e) {
+    e.stopImmediatePropagation();
+    if (this.className.includes("is-active")) {
+      $(".navbar-burger").removeClass("is-active");
+      $(".navbar-menu").removeClass("is-active");
+    } else {
+      $(".navbar-burger").addClass("is-active");
+      $(".navbar-menu").addClass("is-active");
+    }
   });
 });
