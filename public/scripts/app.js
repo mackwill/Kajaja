2342800770461930296515094fec49419c6f189c
William <mack.mindenhall@gmail.com>
2020-07-29 21:15:36 +0000

    Fixed issue with sending message from listing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,7 @@ $(document).ready(() => {
     e.stopImmediatePropagation();
     const formData = $(this).parent().serialize();
     $.post(`/api/messages/${messageThread}`, formData)
-      .then((x) => {
+      .then(() => {
         $("#message-thread").load(
           `/api/messages/${messageThread} #message-thread`
         );
@@ -17,30 +17,39 @@ $(document).ready(() => {
       });
   });
 
-    $(document).on('click', '.delete_listing', function(e){
-      e.preventDefault()
-      e.stopImmediatePropagation()
-      const listingId = $(this).attr('id')
-      $.ajax({
-        url:`/api/widgets/listings/${listingId}`,
-        type:'DELETE',
-        success: function(result){
-          console.log('item deleted', success)
-        }
-      })
-    })
-
+  $(document).on("click", ".delete_listing", function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    const listingId = $(this).attr("id");
+    $.ajax({
+      url: `/api/widgets/listings/${listingId}`,
+      type: "DELETE",
+      success: function (result) {
+        console.log("item deleted", success);
+      },
+    });
+  });
 
   $(document).on("click", ".favourite-link", function (e) {
-    const listingId = $(this).attr('id')
+    const listingId = $(this).attr("id");
     e.preventDefault();
     e.stopImmediatePropagation();
     $.ajax({
-      url:`/api/widgets/favourites/${listingId}`,
-      type:'POST',
-      success: function(result){
-        console.log('it faved', result)
-      }
-    })
+      url: `/api/widgets/favourites/${listingId}`,
+      type: "POST",
+      success: function (result) {
+        console.log("it faved", result);
+      },
+    });
+  });
+
+  $(document).on("click", "#submit-message", function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    const formData = $("#contact-seller-form").serialize();
+    $.post(`/api/messages`, formData).then(() => {
+      $("#contact-seller-form").hide();
+      $("#message-sent-alert").show();
+    });
   });
 });
