c98b9f7b4e2fd94d0f5a1f72c5b3aa0c8fcb83fb
Vincent Bedard-Legault <bedard_v@hotmail.fr>
2020-07-29 17:55:08 +0000

    starting test for upload

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require("morgan");
 const methodOverride = require('method-override');
+//const _ = require('lodash');
+//const fileUpload = require('express-fileupload');
+//const cors = require('cors')
 
 
 // PG database client/connection setup
@@ -29,6 +32,7 @@ const database = require('./database')
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
 app.use(methodOverride('_method'));
+//app.use(fileUpload({createParentPath: true}));
 
 app.use(
   cookieSession({
@@ -40,6 +44,7 @@ app.use(
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
 app.use(
   "/styles",
   sass({
@@ -50,6 +55,8 @@ app.use(
   })
 );
 app.use(express.static("public"));
+app.use(express.static('uploads'));
+
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -111,6 +118,70 @@ app.get("/", (req, res) => {
   templateVars.recentlyViewed = null
   console.log('got no session cookie', templateVars)
   res.render("index", templateVars);
+  }
+});
+
+app.post('/upload-avatar', async(req, res) => {
+  try{
+    if(!req.file){
+      res.send({
+        status:false,
+        message:'No file uploaded'
+      })
+    }else{
+      let avatar = req.files.avatar
+
+      avatar.mv('./uploads/'+ avatar.name)
+
+      res.send({
+        status:true,
+        message:'File is uploaded',
+        data: {
+          name: avatar.name,
+          mimetype: avatar.mimetype,
+          size: avatar.size
+        }
+      })
+    }
+  } catch (err){
+    res.status(500).send(err)
+  }
+})
+
+app.post('/upload-photos', async (req, res) => {
+  try {
+      if(!req.files) {
+          res.send({
+              status: false,
+              message: 'No file uploaded'
+          });
+      } else {
+          let data = [];
+
+          //loop all files
+          _.forEach(_.keysIn(req.files.photos), (key) => {
+              let photo = req.files.photos[key];
+
+              //move photo to uploads directory
+              photo.mv('./uploads/' + photo.name);
+
+              //push file details
+              data.push({
+                  name: photo.name,
+                  mimetype: photo.mimetype,
+                  size: photo.size
+              });
+          });
+
+          //return response
+          res.send({
+              status: true,
+              message: 'Files are uploaded',
+              data: data
+          });
+      }
+  } catch (err) {
+      res.status(500).send(err);
   }
 });
 
